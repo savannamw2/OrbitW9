63b468c2ba85ae311234ed56334f9f8c84cd6adf
Sara Nuss <35274866+scfisk@users.noreply.github.com>
2025-03-07 14:54:16 -0500

    Empty testGPS

---
+++ testGPS.h
@@ -0,0 +1,25 @@
+//
+//  testGPS.h
+//  CSE231_Orbit09
+//
+//  Created by Sara Fisk on 3/7/25.
+//
+
+#pragma once
+
+
+#include "angle.h"
+#include "velocity.h"
+#include "satellite.h"
+#include "gps.h"
+//#include "unitTest.h"
+#include <math.h>
+
+using namespace std;
+
+
+class TestGPS
+{
+    
+};
+
