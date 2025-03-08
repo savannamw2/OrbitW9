c40718a0d5bf2ab6300eca739d0f2f96e20c9c03
Isabel Weaver <59878018+isabelweaver95@users.noreply.github.com>
2025-03-07 19:55:20 -0700

    Add files via upload
    Updated the GPS testing with tests. Need to impliment move

--- testGPS.h
+++ testGPS.h
@@ -12,7 +12,7 @@
 #include "velocity.h"
 #include "satellite.h"
 #include "gps.h"
-//#include "unitTest.h"
+#include "unitTest.h"
 #include <math.h>
 
 using namespace std;
@@ -20,6 +20,329 @@ using namespace std;
 
 class TestGPS
 {
+public:
+    void run()
+    {
+        defaultConstructor();
+        initalConstructor();
+        initalConstructor2();
+        getRadiusMax();
+        getRadiusMin();
+        getRadiusMed();
+        isDeadTrue();
+        isDeadFalse();
+        getPosMin();
+        getPosMed();
+        getPosMax();
+        moveSat1();
+        moveSat2();
+        moveSat3();
+        
+        report("Satellite");
+    }
+
+private:
+
+    /*****************************************************************
+     *****************************************************************
+     * CONSTRUCTOR
+     *****************************************************************
+     *****************************************************************/
+
+     /*********************************************
+      * name:    DEFAULT CONSTRUCTOR
+      * input:   nothing
+      * output:  zero
+      *********************************************/
+    void defaultConstructor()
+    {  // setup
+       // exercise
+        
+        GPS gps;
     
+        // verify
+        assertUnit(gps.angularVelocity == 0.0);
+        assertUnit(gps.dead == false);
+        assertUnit(gps.radius == 0.0);
+    }  // teardown
+
+    /*********************************************
+     * name:    DEFAULT CONSTRUCTOR
+     * input:   nothing
+     * output:  zero
+     *********************************************/
+   void initalConstructor()
+   {  // setup
+       Velocity v;
+       v.dx = 0.0;
+       v.dy = 0.0;
+       Position pos;
+       pos.x = 0.0;
+       pos.y = 0.0;
+       Angle angle;
+       angle.radius = 0.0;
+       
+      // exercise
+       GPS gps(pos, v, angle, 0.0, 0.0);
+       // verify
+       assertUnits(gps.pos.x == 0.0);
+       assertUnits(gps.pos.y == 0.0);
+       assertUnits(gps.velocity.dx == 0.0);
+       assertUnits(gps.velocity.dy == 0.0);
+       assertUnit(gps.angularVelocity == 0.0);
+       assertUnit(gps.dead == false);
+       assertUnit(gps.radius == 0.0);
+   }  // teardown
+    
+    /*********************************************
+     * name:    DEFAULT CONSTRUCTOR
+     * input:   nothing
+     * output:  zero
+     *********************************************/
+   void initalConstructor2()
+   {  // setup
+       Velocity v;
+       v.dx = 0.0;
+       v.dy = 0.0;
+       Position pos;
+       pos.x = 0.0;
+       pos.y = 0.0;
+       Angle angle;
+       angle.radius = 0.0;
+       
+      // exercise
+       GPS gps(pos, v, angle);
+       // verify
+       assertUnits(gps.pos.x == 0.0);
+       assertUnits(gps.pos.y == 0.0);
+       assertUnits(gps.velocity.dx == 0.0);
+       assertUnits(gps.velocity.dy == 0.0);
+       assertUnit(gps.angularVelocity == 0.0);
+       assertUnit(gps.dead == false);
+       assertUnit(gps.radius == 0.0);
+   }  // teardown
+    
+    void copyConstructor()
+    {
+        // setup
+        Velocity v;
+        v.dx = 9.0;
+        v.dy = 9.0;
+        Position pos;
+        pos.x = 9.0;
+        pos.y = 9.0;
+        
+        GPS gps1;
+        gps1.pos = pos;
+        gps1.velocity = v;
+        
+        // exercise
+        GPS gps(s1);
+
+        // verify
+        assertUnits(gps.pos.x == 9.0);
+        assertUnits(gps.pos.y == 9.0);
+        assertUnits(gps.velocity.dx == 9.0);
+        assertUnits(gps.velocity.dy == 9.0);
+        assertUnit(gps.angularVelocity == 0.0);
+        assertUnit(gps.dead == false);
+        assertUnit(gps.radius == 0.0);
+    }
+     /*********************************************
+      * name:   GET RADIUS
+      * input:   nothing
+      * output:  10
+      *********************************************/
+    void getRadiusMax()
+    {  // setup
+        GPS gps;
+        gps.radius = 10.0
+        float rad;
+        
+        // exercise
+        rad = gps.getRadius();
+        
+        // verify
+        assertEquals(rad, 10.0);
+    }  // teardown
+
+    /*********************************************
+     * name:   GET RADIUS
+     * input:   nothing
+     * output:  4.0
+     *********************************************/
+   void getRadiusMin()
+   {  // setup
+       GPS gps;
+       s.radius = 4.0
+       float rad;
+       
+       // exercise
+       rad = gps.getRadius();
+       
+       // verify
+       assertEquals(rad, 4.0);
+   }  // teardown
+
+    /*********************************************
+     * name:   GET RADIUS
+     * input:   nothing
+     * output:  7.0
+     *********************************************/
+   void getRadiusMed()
+   {  // setup
+       GPS gps;
+       gps.radius = 7.0
+       float rad;
+       
+       // exercise
+       rad = gps.getRadius();
+       
+       // verify
+       assertEquals(rad, 7.0);
+   }  // teardown
+    
+    /*********************************************
+     * name:   IS DEAD TRUE
+     * input:   nothing
+     * output:  TRUE
+     *********************************************/
+   void isDeadTrue()
+   {  // setup
+       GPS gps;
+       gps.dead = true;
+       bool dead;
+       
+       // exercise
+       dead = gps.isDead();
+       
+       // verify
+       assertEquals(dead, true);
+   }  // teardown
+    
+    /*********************************************
+     * name:   IS DEAD FALSE
+     * input:   nothing
+     * output:  TRUE
+     *********************************************/
+   void isDeadFalse()
+   {  // setup
+       GPS gps;
+       gps.dead = false;
+       bool dead;
+       
+       // exercise
+       dead = gps.isDead();
+       
+       // verify
+       assertEquals(dead, false);
+   }  // teardown
+    
+    /*********************************************
+     * name:   GET POS
+     * input:   nothing
+     * output:
+     *********************************************/
+   void getPosMin()
+   {  // setup
+       GPS gps;
+
+       gps.pos.x = 0.0;
+       gps.pos.y = 0.0;
+       
+       Posistion p;
+       p.x = 99.0;
+       p.y = 99.0;
+       // exercise
+       p = gps.getPosition();
+       
+       // verify
+       assertEquals(p.x, 0.0);
+       assertEquals(p.y, 0.0);
+       assertEquals(gps.pos.x, 0.0);
+       assertEquals(gps.pos.y, 0.0);
+   }  // teardown
+    
+    /*********************************************
+     * name:   GET POS
+     * input:   nothing
+     * output:
+     *********************************************/
+   void getPosMed()
+   {  // setup
+       GPS gps;
+
+       gps.pos.x = 50.0;
+       gps.pos.y = 50.0;
+       
+       Posistion p;
+       p.x = 99.0;
+       p.y = 99.0;
+       // exercise
+       p = gps.getPosition();
+       
+       // verify
+       assertEquals(p.x, 50.0);
+       assertEquals(p.y, 50.0);
+       assertEquals(gps.pos.x, 50.0);
+       assertEquals(gps.pos.y, 50.0);
+   }  // teardown
+    
+    /*********************************************
+     * name:   GET POS
+     * input:   nothing
+     * output:
+     *********************************************/
+   void getPosMax()
+   {  // setup
+       GPS gps;
+
+       gps.pos.x = 100.0;
+       gps.pos.y = 100.0;
+       
+       Posistion p;
+       p.x = 99.0;
+       p.y = 99.0;
+       // exercise
+       p = gps.getPosition();
+       
+       // verify
+       assertEquals(p.x, 100.0);
+       assertEquals(p.y, 100.0);
+       assertEquals(gps.pos.x, 100.0);
+       assertEquals(gps.pos.y, 100.0);
+   }  // teardown
+    
+    /*********************************************
+     * name:   Move
+     * input:   nothing
+     * output:
+     *********************************************/
+   void moveSat1()
+   {  // setup
+       
+       
+   }  // teardown
+    
+    /*********************************************
+     * name:   MOVE
+     * input:   nothing
+     * output:
+     *********************************************/
+   void moveSat2()
+   {  // setup
+       
+       
+   }  // teardown
+    
+    /*********************************************
+     * name:  MOVE
+     * input:   nothing
+     * output:
+     *********************************************/
+   void moveSat3()
+   {  // setup
+       
+   }  // teardown
 };
 
